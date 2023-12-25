c0339b59771c25c37dc51d05107436de15c11483
unknown <smartkelvin66@gmail.com>
2023-12-25 14:46:45 +0100

    smart

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,26 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
-import { useColorScheme } from "react-native";
-import { PermissionsAndroid, Platform } from "react-native";
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { PermissionsAndroid, Platform, useColorScheme } from 'react-native';
+import { StreamVideo } from '@stream-io/video-react-native-sdk';
+import { StreamClientProvider } from '../lib/stream';
+import { AuthProvider } from '../context/AuthProvider';
+import { CallsProvider } from '../context/CallsProvider';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from "expo-router";
+} from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -25,7 +28,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
@@ -42,16 +45,15 @@ export default function RootLayout() {
 
   useEffect(() => {
     const run = async () => {
-      if (Platform.OS === "android") {
+      if (Platform.OS === 'android') {
         await PermissionsAndroid.requestMultiple([
-          "android.permission.POST_NOTIFICATIONS",
-          "android.permission.BLUETOOTH_CONNECT",
+          'android.permission.POST_NOTIFICATIONS',
+          'android.permission.BLUETOOTH_CONNECT',
         ]);
       }
     };
     run();
   }, []);
-  //smart
 
   if (!loaded) {
     return null;
@@ -64,11 +66,18 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </ThemeProvider>
+    <AuthProvider>
+      <StreamClientProvider>
+        <CallsProvider>
+          <ThemeProvider
+            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+          >
+            <Stack>
+              <Stack.Screen name="(app)" options={{ headerShown: false }} />
+            </Stack>
+          </ThemeProvider>
+        </CallsProvider>
+      </StreamClientProvider>
+    </AuthProvider>
   );
 }
